eac81aa964547c84b76f46853c1e17519ec91424
Fichtner, Florian Willy <florian.fichtner@dlr.de>
2021-02-22 10:37:06 +0100

    added license & description

--- python-client/setup.py
+++ python-client/setup.py
@@ -6,6 +6,11 @@ if __name__ == "__main__":
     setuptools.setup(
         name="min_creds_client",
         version="0.1.1",
+        url="https://github.com/dlr-eoc/ukis-min_creds",
+        author="German Aerospace Center (DLR)",
+        author_email="ukis-helpdesk@dlr.de",
+        license="Apache 2.0",
+        description="client library for ukis-min_creds",
         python_requires='>=3.6',
         packages=setuptools.find_packages(),
         install_requires=[
